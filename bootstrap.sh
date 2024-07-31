6be419ffd904c0a887b165193b38e4c109b79d22
Deofex <28751252+Deofex@users.noreply.github.com>
2024-07-31 12:52:23 +0200

    add user account creaction

--- bootstrap.sh
+++ bootstrap.sh
@@ -30,6 +30,18 @@ openssl x509 -req -in cert_csr.pem -sha256 -days 365 -extensions v3_ca -signkey 
 kubectl create secret tls argocd-server-tls --key="cert_key.pem" --cert="cert_cert.pem" -n nginx-shared-gateway
 rm cert_key.pem  cert_cert.pem cert_csr.pem
 
+# Create user account with the password Welkom01:
+kubectl patch configmap argocd-cm -n argocd \
+  --type='json' -p='[{"op": "add", "path": "/data/accounts.user", "value": "login"}]'
+kubectl patch secret argocd-secret   -n argocd \
+  --type='json' -p='[{"op": "add", "path": "/data/accounts.user.password", "value": "JDJhJDEwJFBxNGVrd2dyMTV5QjRSSlFZZkNvbi5CR1dlSXBINTBtNklYMVNkNjBrLnhrSmFScFZZVE1P"}]'
+kubectl patch secret argocd-secret   -n argocd \
+  --type='json' -p='[{"op": "add", "path": "/data/accounts.user.passwordMtime", "value": "MjAyNC0wNy0zMVQxMDozNDoyM1o="}]'
+kubectl patch secret argocd-secret   -n argocd \
+  --type='json' -p='[{"op": "add", "path": "/data/accounts.user.tokens", "value": "bnVsbA=="}]'
+
+
+
 # Provide credentials to user
 echo "ARGO CD is accessible with the following credentials: \"admin\", password: \"$(kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d)\""
 
